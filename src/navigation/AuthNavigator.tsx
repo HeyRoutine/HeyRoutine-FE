90a5c4bc4db7dd80c2ac3f07a483704559ab9336
김응서 <ksy_624@naver.com>
2025-08-13 15:47:55 +0900

    feat[#2]: EmailInputScreen 구조 완성

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from '../screens/auth/SplashScreen';
 import LoginScreen from '../screens/auth/LoginScreen';
 import EmailLoginScreen from '../screens/auth/EmailLoginScreen';
+import EmailInputScreen from '../screens/auth/email-signup/EmailInputScreen';
 
 const Stack = createNativeStackNavigator();
 const AuthNavigator = () => {
@@ -16,6 +17,9 @@ const AuthNavigator = () => {
       {/* <Stack.Screen name="Splash" component={SplashScreen} /> */}
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="EmailLoginScreen" component={EmailLoginScreen} />
+      <Stack.Screen name="EmailInputScreen" component={EmailInputScreen} />
+      {/* <Stack.Screen name="VerificationScreen" component={VerificationScreen} /> */}
+      {/* <Stack.Screen name="PasswordResetScreen" component={PasswordResetScreen} /> */}
     </Stack.Navigator>
   );
 };
