5df5b1372edf10a1f188fa19b5b29b03cacbfe0f
박규빈 <gyubin@baggyubin-ui-MacBookAir.local>
2025-08-16 22:06:46 +0900

    feat[#2]: PasswordScreen Title 두께 변경

--- src/screens/auth/email-signup/PasswordScreen.tsx
+++ src/screens/auth/email-signup/PasswordScreen.tsx
@@ -123,7 +123,7 @@ const Content = styled.View`
 
 const Title = styled.Text`
   font-size: 24px;
-  font-family: ${theme.fonts.Bold};
+  font-family: ${theme.fonts.SemiBold};
   color: ${theme.colors.gray900};
   line-height: 34px;
   margin-top: 16px;
