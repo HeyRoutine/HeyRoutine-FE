92e2f6496c4c94c1d6e72bd6009c4a5c64077744
박규빈 <gyubin@baggyubin-ui-MacBookAir.local>
2025-08-19 01:12:39 +0900

    feat[#26]: AnalysisStack 페이지 import 순서 정리

--- src/navigation/stack/AnalysisStack.tsx
+++ src/navigation/stack/AnalysisStack.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AnalysisScreen from '../../screens/analysis/AnalysisScreen';
-import ConsumptionAnalysisScreen from '../../screens/analysis/ConsumptionAnalysisScreen';
 import FinancialProductScreen from '../../screens/analysis/FinancialProductScreen';
 import LoadingScreen from '../../screens/common/LoadingScreen';
+import ConsumptionAnalysisScreen from '../../screens/analysis/ConsumptionAnalysisScreen';
 import RoutineSuggestionScreen from '../../screens/analysis/RoutineSuggestionScreen';
 
 const Stack = createNativeStackNavigator();
