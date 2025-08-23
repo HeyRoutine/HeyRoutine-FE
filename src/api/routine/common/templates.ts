29e329b26f159eeba7a053e26b4d8f2956baf954
BEAR <ungnam0509@gmail.com>
2025-08-23 18:11:52 +0900

    refactor[#52]: 루틴 공통 API 리팩토링

---
+++ src/api/routine/common/templates.ts
@@ -0,0 +1,25 @@
+import apiClient from '../../client';
+import {
+  ApiResponse,
+  RoutineTemplateListResponse,
+  RoutineTemplateListParams,
+} from '../../../types/api';
+
+// 루틴 템플릿 조회 API
+export const getRoutineTemplate = async (
+  params: RoutineTemplateListParams = {},
+): Promise<ApiResponse<RoutineTemplateListResponse>> => {
+  const { category, page = 0, size = 10 } = params;
+
+  const response = await apiClient.get<
+    ApiResponse<RoutineTemplateListResponse>
+  >('/api/v1/routines/templates', {
+    params: {
+      ...(category && { category }),
+      page,
+      size,
+    },
+  });
+
+  return response.data;
+};
